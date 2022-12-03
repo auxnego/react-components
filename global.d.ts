c4ac563ba84cf046f5bc0cc3b483b032bc55db6c
Esdras Cordero <josuequeraleswd2420@gmail.com>
2022-12-03 18:52:40 -0400

    fix: Update global types

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,16 @@
+import { ComponentClass, FC} from "react";
+
+export type BasicConfig = {
+  setEl?: string|FC|ComponentClass
+};
+
+export type Config<ClassName = {}, Props = {}, Element = {}> = {
+  setEl?:Element,
+  setProp?:Props,
+  setClas:ClassName
+};
+
+
 
 export type ClassNameConfig = {
   classless?:boolean,
