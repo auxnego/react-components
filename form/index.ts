f12f68fdf1e7a7f59c1402a0745b86f438001b0e
Esdras Cordero <josuequeraleswd2420@gmail.com>
2022-12-06 23:05:14 -0400

    fix(form): Update index

--- form/index.ts
+++ form/index.ts
@@ -1,11 +1,11 @@
 import LabelWrapper from "./label-wrapper";
 import Input from "./input";
 import CheckboxBase from "./checkbox-base";
-import RadioBase from "./radio-base";
+//import RadioBase from "./radio-base";
 
 export {
   CheckboxBase,
   Input,
   LabelWrapper,
-  RadioBase
+ // RadioBase
 };
